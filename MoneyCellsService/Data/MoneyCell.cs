2b0bc8ab4498222e0c42a61178c407d8ec9f38f9
Zuev Alexey <zuevalexey71@gmail.com>
2018-04-29 20:45:21 +0300

    добавил ссылку линк2дб + классы моделей данных

---
+++ MoneyCellsService/Data/MoneyCell.cs
@@ -0,0 +1,62 @@
+﻿using LinqToDB.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyCellsService.Data {
+   /// <summary>
+   /// Объект-сущность СУБД, представляющий денежную ячейку
+   /// </summary>
+   [Table("MoneyCells")]
+   public class MoneyCell {
+      /// <summary>
+      /// Идентификатор
+      /// </summary>
+      [PrimaryKey,Identity]
+      public long Id { get; set; }
+
+      /// <summary>
+      /// Идентификатор владельца
+      /// </summary>
+      [Column,NotNull]
+      public long OwnerId { get; set; }
+
+      /// <summary>
+      /// Дата создания
+      /// </summary>
+      [Column,NotNull]
+      public DateTime CreationDate {get;set;}
+
+      /// <summary>
+      /// Тип
+      /// </summary>
+      [Column,NotNull]
+      public byte Type { get; set; }
+
+      /// <summary>
+      /// Тип валюты
+      /// </summary>
+      [Column,NotNull]
+      public byte CurrencyType { get; set; }
+
+      /// <summary>
+      /// Баланс
+      /// </summary>
+      [Column,NotNull]
+      public float Balance { get;set;}
+
+      /// <summary>
+      /// Статус
+      /// </summary>
+      [Column,NotNull]
+      public byte Status { get;set;}
+
+      /// <summary>
+      /// Имя
+      /// </summary>
+      [Column,NotNull]
+      public string Name { get; set; }
+   }
+}
---
+++ MoneyCellsService/Data/Transaction.cs
@@ -0,0 +1,50 @@
+﻿using LinqToDB.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyCellsService.Data {
+   /// <summary>
+   /// Объект-сущность СУБД, представляющий валютрую транзакцию
+   /// </summary>
+   [Table("Transactions")]
+   public class Transaction {
+      /// <summary>
+      /// Идентификатор
+      /// </summary>
+      [PrimaryKey,Identity]
+      public long Id { get; set; }
+
+      /// <summary>
+      /// Идентификатор ячейки-источника
+      /// </summary>
+      [Column,NotNull]
+      public long From {get;set;}
+
+      /// <summary>
+      /// Идентификатор ячейки-получателя
+      /// </summary>
+      [Column,NotNull]
+      public long To {get;set;}
+
+      /// <summary>
+      /// Размер
+      /// </summary>
+      [Column,NotNull]
+      public float Amount {get;set;}
+
+      /// <summary>
+      /// Дата
+      /// </summary>
+      [Column,NotNull]
+      public DateTime Date {get;set;}
+
+      /// <summary>
+      /// Результат
+      /// </summary>
+      [Column,NotNull]
+      public byte Status {get;set;}
+   }
+}
