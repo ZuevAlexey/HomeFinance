e815e0a0e3380236197228a097bc06fcee422204
Zuev Alexey <zuevalexey71@gmail.com>
2018-05-09 20:14:58 +0300

    Контракты сервиса группировок

---
+++ GroupService/GroupServiceContracts/Data/Group.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace MyCompany.Services.Groups.Contracts.Data {
+   /// <summary>
+   /// Класс представляющий группу объектов
+   /// </summary>
+   public class Group {
+      /// <summary>
+      /// Идентификатор группы
+      /// </summary>
+      public long Id { get; set;}
+
+      /// <summary>
+      /// Имя группы
+      /// </summary>
+      public string Name { get;set;}
+
+      /// <summary>
+      /// Идентификаторы членов группы
+      /// </summary>
+      public HashSet<long> Children{ get;set;}
+   }
+}
---
+++ GroupService/GroupServiceContracts/IGroupService.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using MyCompany.Services.Groups.Contracts.Data;
+
+namespace MyCompany.Services.Groups.Contracts {
+   /// <summary>
+   /// Интерфейс сервиса группировки
+   /// </summary>
+   public interface IGroupService {
+      /// <summary>
+      /// Обновить данные о группах
+      /// </summary>
+      /// <param name="groups">Колекция групп, которых нужно обновить</param>
+      /// <returns>Содержит идентификаторы для успешно обновленных записей и -1 для ошибочных</returns>
+      ICollection<long> UpsertGroups(IEnumerable<Group> groups);
+
+      /// <summary>
+      /// Получить группы по идентификаторам
+      /// </summary>
+      /// <param name="ids">Идентификаторы искомых групп</param>
+      /// <returns>Коллекция найденных групп</returns>
+      ICollection<Group> GetGroups(IEnumerable<long> ids);
+
+      /// <summary>
+      /// Добавить объекты в группу
+      /// </summary>
+      /// <param name="groupId">Идентификатор группы, в которую добавляем объекты</param>
+      /// <param name="objectsIds">Идентификаторы добавляемых объектов</param>
+      /// <returns>Коллекция, содержащая true - для успешно добавленного объекта (либо уже в группе состоявшего), false - в случае ошибки добавления</returns>
+      ICollection<bool> AddInGroup(long groupId, IEnumerable<long> objectsIds);
+
+      /// <summary>
+      /// Исключить объекты из группы
+      /// </summary>
+      /// <param name="groupId">Идентификатор группы, из которой исключаем объекты</param>
+      /// <param name="objectsIds">Идентификаторы исключаемых объектов</param>
+      /// <returns>Коллекция, содержащая true - для успешно исключенного объекта (либо уже в группе не состоявшего), false - в случае ошибки исключения</returns>
+      ICollection<bool> RemoveFromGroup(long groupId, IEnumerable<long> objectsIds);
+
+      /// <summary>
+      /// Получить членов группы по идентификатору группы
+      /// </summary>
+      /// <param name="groupId">Идентификатор группы</param>
+      /// <returns>Список идентификаторов объектов, входящих в группу</returns>
+      ICollection<long> GetGroupList(long groupId);
+    }
+}
