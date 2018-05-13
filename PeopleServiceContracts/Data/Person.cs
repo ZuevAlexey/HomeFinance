dc0fcbe965130ef65b2a005afc331b9ab6e151c3
Zuev Alexey <zuevalexey71@gmail.com>
2018-05-13 18:16:19 +0300

    Контракты сервиса людей

--- PeopleServiceContracts/Data/Person.cs
+++ PeopleServiceContracts/Data/Person.cs
@@ -2,7 +2,7 @@
 
 namespace MyCompany.Services.People.Contracts.Data {
    public class Person {
-      public long PersonId { get; set; }
+      public long Id { get; set; }
       public string FirstName { get; set; }
       public string LastName { get; set; }
       public string MiddleName { get; set; }
--- PeopleServiceContracts/IPeopleService.cs
+++ PeopleServiceContracts/IPeopleService.cs
@@ -1,3 +1,23 @@
-﻿namespace MyCompany.Services.People.Contracts {
-   public interface IPeopleService { }
+﻿using System.Collections.Generic;
+using MyCompany.Services.People.Contracts.Data;
+
+namespace MyCompany.Services.People.Contracts {
+   /// <summary>
+   /// Интерфейс для работы с сервисом людей
+   /// </summary>
+   public interface IPeopleService {
+      /// <summary>
+      /// Получить людей по идентификаторам
+      /// </summary>
+      /// <param name="ids">Коллекция идентификаторов</param>
+      /// <returns>Коллекция людей</returns>
+      ICollection<Person> GetPeople(IEnumerable<long> ids);
+
+      /// <summary>
+      /// Обновить данные о людях
+      /// </summary>
+      /// <param name="people">Коллекция людей для сохранения</param>
+      /// <returns>Коллекция идентификаторов для успешно сохраненных людей, для ошибочно сохраненных -1</returns>
+      ICollection<long> Upsert(IEnumerable<Person> people);
+   }
 }
