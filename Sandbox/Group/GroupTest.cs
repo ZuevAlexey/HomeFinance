1221dda9d8a373a5954dc95a8a4d51628587eae4
Zuev Alexey <zuevalexey71@gmail.com>
2018-05-20 16:52:50 +0300

    правка сендбокса

---
+++ Sandbox/Group/GroupTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using MyCompany.Services.Groups.Service.Data.Entities;
+using MyCompany.Services.Groups.Service.Data.Provider;
+
+namespace Sandbox.Group {
+   internal static class GroupTest {
+      internal static void Run() {
+         var provider = new GroupProvider();
+         var group = CreateEmptyGroup("first");
+         var groups = GetGroups();
+         var res = provider.UpsertGroups(groups);
+
+         var result = provider.GetGroups(new long[] {1}).ToList();
+      }
+
+      private static GroupEntity CreateEmptyGroup(string name) {
+         return new GroupEntity {Name = name};
+      }
+
+      private static IEnumerable<GroupEntity> GetGroups() {
+         var group = new GroupEntity {
+            Name = "Second"
+         };
+         var children = new List<ChildEntity> {
+            new ChildEntity {
+               Id = 1,
+               GroupId = group.Id
+            },
+            new ChildEntity {
+               Id = 2,
+               GroupId = group.Id
+            },
+            new ChildEntity {
+               Id = 3,
+               GroupId = group.Id
+            }
+         };
+         group.Children = children;
+
+         return new[] {group};
+      }
+   }
+}
---
+++ Sandbox/MoneyCells/TransactionTest.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JRPC.Client;
+using MyCompany.Services.MoneyCells.Contracts;
+using MyCompany.Services.MoneyCells.Contracts.Data;
+using MyCompany.Services.MoneyCells.Contracts.Enums;
+using MyCompany.Services.MoneyCells.Contracts.Seacrh;
+
+namespace Sandbox.MoneyCells {
+   public static class TransactionTest {
+      private const string PRETTY_LINE = "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*";
+
+      public static void Run() {
+         var client = new JRpcClient("http://127.0.0.1:15555");
+         var moneyCellsService = client.GetProxy<IMoneyCellsService>("MoneyCellsService");
+         while (true) {
+            ShowMoneyCellsInfo(moneyCellsService);
+            Console.WriteLine(PRETTY_LINE);
+            Console.WriteLine("С какого счета будем списывать бабки?");
+            if (!long.TryParse(Console.ReadLine(), out var fromId)) {
+               Console.WriteLine("Идентификатор счета - целое число.");
+               continue;
+            }
+
+            Console.WriteLine("На какой положим?");
+            if (!long.TryParse(Console.ReadLine(), out var toId)) {
+               Console.WriteLine("Идентификатор счета - целое число.");
+               continue;
+            }
+
+            Console.WriteLine("Сколько?");
+            if (!long.TryParse(Console.ReadLine(), out var amount)) {
+               Console.WriteLine("Идентификатор счета - целое число.");
+               continue;
+            }
+
+            Console.WriteLine(PRETTY_LINE);
+            var transaction = ProcessTransaction(moneyCellsService, fromId, toId, amount);
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Данные о транзакции :");
+            sb.AppendLine($"Идентификатор получателя : {transaction.To}");
+            sb.AppendLine($"Идентификатор отправителя : {transaction.From}");
+            sb.AppendLine($"Дата транзакции : {transaction.Date}");
+            sb.AppendLine($"Сумма : {transaction.Amount}");
+            sb.AppendLine($"Статус : {transaction.Status}");
+            Console.WriteLine(sb.ToString());
+            Console.WriteLine(PRETTY_LINE);
+            Console.WriteLine("Еще разок (y/n)?");
+            if (Console.ReadKey().Key == ConsoleKey.N) {
+               Console.WriteLine();
+               break;
+            }
+         }
+      }
+
+      private static Transaction ProcessTransaction(IMoneyCellsService moneyCellsService, long fromId, long toId,
+         long amount) {
+         if (fromId == default(long)) {
+            return moneyCellsService.Replenish(toId, amount);
+         }
+
+         return toId == default(long)
+            ? moneyCellsService.WithDraw(fromId, amount)
+            : moneyCellsService.ProcessTransaction(fromId, toId, amount);
+      }
+
+      private static void ShowMoneyCellsInfo(IMoneyCellsService moneyCellsService) {
+         Console.WriteLine("Инфа по картонкам:");
+         var filter = new MoneyCellFilter {Statuses = new HashSet<MoneyCellStatus> {MoneyCellStatus.Active}};
+         var moneyCells = moneyCellsService.Get(filter);
+         foreach (var cell in moneyCells) {
+            Show(cell);
+         }
+
+         Console.WriteLine(PRETTY_LINE);
+      }
+
+      private static void Show(MoneyCell cell) {
+         Console.WriteLine(PRETTY_LINE);
+         var sb = new StringBuilder();
+         sb.AppendLine($"{cell.Name}:");
+         sb.AppendLine($"Идентификатор : {cell.Id} ({cell.Type})");
+         sb.AppendLine($"Баланс : {cell.Balance} {cell.Currency}");
+         Console.WriteLine(sb.ToString());
+      }
+
+      private static List<MoneyCell> GetMoneyCells() {
+         var insert = new List<MoneyCell> {
+            new MoneyCell {
+               Balance = 1100,
+               CreationDate = DateTime.Now,
+               Currency = Currency.RUB,
+               Name = "Кошелек",
+               OwnerId = 1,
+               Status = MoneyCellStatus.Active,
+               Type = MoneyCellType.Сash,
+               IsDeleted = false
+            },
+            new MoneyCell {
+               Balance = 1,
+               CreationDate = DateTime.Now,
+               Currency = Currency.USD,
+               Name = "Наташкина заначка",
+               OwnerId = 2,
+               Status = MoneyCellStatus.Active,
+               Type = MoneyCellType.Сash,
+               IsDeleted = false
+            },
+            new MoneyCell {
+               Balance = 150000,
+               CreationDate = DateTime.Now,
+               Currency = Currency.RUB,
+               Name = "Для вклада в Восточный банк",
+               Status = MoneyCellStatus.Active,
+               IsDeleted = false,
+               OwnerId = 1,
+               Type = MoneyCellType.Сash
+            },
+            new MoneyCell {
+               Balance = 605000,
+               CreationDate = new DateTime(2018, 4, 10),
+               Status = MoneyCellStatus.Active,
+               IsDeleted = false,
+               Currency = Currency.RUB,
+               Name = "Вклад в восточном банке",
+               OwnerId = 1,
+               Type = MoneyCellType.Deposit
+            },
+
+            new MoneyCell {
+               Status = MoneyCellStatus.Active,
+               Type = MoneyCellType.Card,
+               Balance = 10000,
+               CreationDate = new DateTime(2018, 4, 2),
+               Currency = Currency.RUB,
+               IsDeleted = false,
+               Name = "Зарплатная карта Сбербанк",
+               OwnerId = 1
+            }
+         };
+         return insert;
+      }
+   }
+}
