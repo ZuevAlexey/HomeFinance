2595530cc4cddf0fb0b5c03cdb2f18730cd85763
Zuev Alexey <zuevalexey71@gmail.com>
2018-05-20 17:23:55 +0300

    ручной тест сервиса группировок

--- Sandbox/Group/GroupTest.cs
+++ Sandbox/Group/GroupTest.cs
@@ -1,42 +1,26 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using MyCompany.Services.Groups.Service.Data.Entities;
-using MyCompany.Services.Groups.Service.Data.Provider;
+using JRPC.Client;
+using MyCompany.Services.Groups.Contracts;
 
 namespace Sandbox.Group {
    internal static class GroupTest {
       internal static void Run() {
-         var provider = new GroupProvider();
-         var group = CreateEmptyGroup("first");
+         var proxy = JRpcClient.Create<IGroupService>("http://localhost:16500");
          var groups = GetGroups();
-         var res = provider.UpsertGroups(groups);
 
-         var result = provider.GetGroups(new long[] {1}).ToList();
+         var res = proxy.UpsertGroups(groups);
+         var list = proxy.GetGroupList(2);
+         var addResult = proxy.AddInGroup(3, new long[] {2, 6, 8});
+         var removeResult = proxy.RemoveFromGroup(2, new long[] {2, 6, 8});
+         var allGroups = proxy.GetGroups(new long[] {1, 2, 3, 4, 5, 6}).ToList();
       }
 
-      private static GroupEntity CreateEmptyGroup(string name) {
-         return new GroupEntity {Name = name};
-      }
-
-      private static IEnumerable<GroupEntity> GetGroups() {
-         var group = new GroupEntity {
-            Name = "Second"
+      private static IEnumerable<MyCompany.Services.Groups.Contracts.Data.Group> GetGroups() {
+         var group = new MyCompany.Services.Groups.Contracts.Data.Group {
+            Name = "Second",
+            Children = new HashSet<long>(new long[] {1, 2})
          };
-         var children = new List<ChildEntity> {
-            new ChildEntity {
-               Id = 1,
-               GroupId = group.Id
-            },
-            new ChildEntity {
-               Id = 2,
-               GroupId = group.Id
-            },
-            new ChildEntity {
-               Id = 3,
-               GroupId = group.Id
-            }
-         };
-         group.Children = children;
 
          return new[] {group};
       }
