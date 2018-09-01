cfc37bee39850fb886bd85f653f38fcc8483fdcd
Zuev Alexey <zuevalexey71@gmail.com>
2018-09-01 13:09:12 +0300

    save

--- AndroidApp/App.js
+++ AndroidApp/App.js
@@ -1,33 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import actionsName from './app/constants/ActionsName';
-import List from './app/components/List';
-import moneyCellState from './app/constants/MoneyCellStates';
-import moneyCellTypes from './app/constants/MoneyCellTypes';
-import sex from './app/constants/SEX';
+import {View, Text} from 'react-native';
 
 export default class App extends React.Component {
+
   render() {
+    let data = [1,2,3,4,5].map(e => e * 2);
     return (
-      <View style={styles.container}>
-        <List name = 'actions' caption = 'Action Names' data = {actionsName} />
-        <Text></Text>
-        <List name = 'states' caption = 'MoneyCell States' data = {moneyCellState} />
-        <Text></Text>
-        <List name = 'types' caption = 'MoneyCell Types' data = {moneyCellTypes} />
-        <Text></Text>
-        <List name = 'sex' caption = 'Sex' data = {sex} />
-        <Text></Text>
-      </View>
+        <View>
+            {data.map((el, index) =>{
+              return <Text key={index}>{el}</Text>
+            })}
+        </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
