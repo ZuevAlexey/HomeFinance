7b33e5a531da0f3f7c88b7cdfd29b377f96d650d
aleksey.zuev <zuevalexey71@gmail.com>
2019-03-04 17:24:15 +0300

    init SyncServer project

---
+++ SyncServer/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SyncServer {
+    internal class Program {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
