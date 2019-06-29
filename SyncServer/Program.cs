47a2a0cb53ca01c7b9c36f858a9e5bd89d4027da
aleksey.zuev <zuevalexey71@gmail.com>
2019-06-29 13:28:31 +0300

    Удалил неиспользуемый функционал

--- SyncServer/Program.cs
+++ SyncServer/Program.cs
@@ -1,10 +1,27 @@
 ﻿using System;
+using Nancy;
+using Nancy.Hosting.Self;
 
 namespace SyncServer {
     internal class Program {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var hostConfigs = new HostConfiguration {
+                UrlReservations = new UrlReservations { CreateAutomatically = true }
+            };
+            var nancyHost = new NancyHost(hostConfigs, new Uri("http://localhost:1234"));
+            nancyHost.Start();
+            Console.WriteLine("Service started!");
+            Console.ReadLine();
+            nancyHost.Stop();
+            Console.WriteLine("Service stoped!");
+        }
+    }
+
+    public class Module: NancyModule {
+        public Module()
+        {
+            Get["/{name}"] = x => string.Concat($"Hello {x.name}");
         }
     }
 }
